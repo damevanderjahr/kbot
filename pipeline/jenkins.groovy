d6ca8d61c8f4128089eaa0a2968b0950fa49a3a5
Dmytro Zharynin <dmzh@solvve.com>
2024-04-21 17:59:10 +0300

    Add jenkins pipeline prototype

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+    parameters {
+
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick Arch')
+
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo "Build for platform ${params.OS}"
+
+                echo "Build for arch: ${params.ARCH}"
+
+                make TARGETOS=${params.OS} TARGETARCH=${params.ARCH} build
+
+            }
+        }
+    }
+}
